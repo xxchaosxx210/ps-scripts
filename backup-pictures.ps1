5a96d54ee27b6d55032e15eb62ae5af5149d42df
xxchaosxx210 <pmillar1279@googlemail.com>
2023-04-10 18:55:49 +0100

    Update backup-pictures.ps1

--- backup-pictures.ps1
+++ backup-pictures.ps1
@@ -1,10 +1,10 @@
-# This script allows to backup pictures in the pictures home folder with a prefix name
-
-
-# 1. get the Pictures full path from the Home Path enviroment variable
-# 2. search the path for images (check extension and add to array)
-
 function Get-Images-From-Path {
+    <#
+    .PARAMETER Path
+    The path to search for images.
+    .NOTES
+    Returns an array of files that are images, recursively from the input path.
+    #>
     Param(
         [string]$Path
     )
@@ -13,14 +13,22 @@ function Get-Images-From-Path {
 }
 
 function New-Backup-Path {
+    <#
+    .PARAMETER BackupPath
+    The name of the directory that will be created for backups.
+    .NOTES
+    Creates a new directory for backup if one with the same name doesn't exist.
+    #>
     Param(
         [string]$BackupPath
     )
+    # check the pathname does not exist before proceeding
     if ((Test-Path -Path $BackupPath) -eq $true) {
         Write-Host "Backup path with that name already exists"
         return $false
     }
     try {
+        # create the new path
         New-Item -Path $BackupPath -ItemType "directory"
         return $true
     }
@@ -32,10 +40,19 @@ function New-Backup-Path {
 }
 
 function Copy-Pictures-To-Backup-Path {
+    <#
+    .PARAMETER BackPath
+    The path where the backup will be stored.
+    .PARAMETER SourceFilePaths
+    An array of source paths for the pictures.
+    .NOTES
+    Copies pictures from source paths to a backup directory.
+    #>
     Param(
         [string]$BackPath,
         [string[]]$SourceFilePaths
     )
+    # keep track of the amount of files copied
     $counter = 0
     foreach ($sourceFilePath in $SourceFilePaths) {
         # split the filename from the path and append to the back path
@@ -55,31 +72,51 @@ function Copy-Pictures-To-Backup-Path {
 
 
 function Show-Main {
+    <#
+    .NOTES
+    Main function that drives the whole process of creating backups for pictures.
+    #>
     # get the full path of the my pictures special folder using the enumerated type
     # check the .NET documentation of GetFolderPath
     $MY_PICTURES_PATH = [System.Environment]::GetFolderPath([Environment+SpecialFolder]::MyPictures)
+    # prompt for backup pathname
     $prefixPathName = Read-Host "Prefix Backup Name? "
+    # create the full backup path
     $backupPath = Join-Path $MY_PICTURES_PATH $prefixPathName
     $newPathResult = New-Backup-Path -BackupPath $backupPath
     if ($newPathResult -eq $false) {
         Write-Output "Could not create Backup Directory"
         return
     }
+    # get all image files from the pictures directory and copy them all to the backup path
     $imageFiles = Get-Images-From-Path -Path $MY_PICTURES_PATH
     $copyCount = Copy-Pictures-To-Backup-Path -BackPath $backupPath -SourceFilePaths $imageFiles
     if ($copyCount -eq 0) {
         return
     }
-    # compress the files
+    # zip the backup path
     $zipBackupPath = $backupPath + ".zip"
+    Write-Output "Creating Backup Zip: ${zipBackupPath}..."
     try {
         [System.IO.Compression.ZipFile]::CreateFromDirectory($backupPath, $zipBackupPath)
     }
     catch {
         Write-Error "Error creating zip file: $_"
+        return
     }
+    # remove the backup folder and its contents
+    try {
+        Remove-Item $backupPath -Recurse -Force
+        Write-Output "Removing Backup Folder..."
+    }
+    catch {
+        Write-Error "Error removing backup folder: $_"
+    }
+    Write-Output "Backup created and compressed!"
 }
 
+# load the .NET library for compressing files
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
+# call the main function
 Show-Main
